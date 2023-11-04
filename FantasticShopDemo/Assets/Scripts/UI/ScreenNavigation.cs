1ea49ff2eb764d5127323f9743012f333a135483
Javier <josejavier@yeltic.com>
2023-11-04 16:01:53 -0600

    Screen navigation

--- FantasticShopDemo/Assets/Scripts/UI/ScreenNavigation.cs
+++ FantasticShopDemo/Assets/Scripts/UI/ScreenNavigation.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class ScreenNavigation : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject firstScreen;
+    private IScreen currentScreen;
+    private List<IScreen> previousScreens = new List<IScreen>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentScreen = firstScreen.GetComponent<IScreen>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowScreen(GameObject screen)
     {
-        
+        IScreen screenToShow = screen.GetComponent<IScreen>();
+        if (currentScreen != null)
+        {
+            currentScreen.Hide();
+            previousScreens.Add(currentScreen);
+        }
+
+        SetCurrentScreen(screenToShow);
+        currentScreen.Show();
+    }
+
+    public void ShowPreviousScreen()
+    {
+      
+        if (previousScreens.Count > 0)
+        {
+            currentScreen.Hide(); //if we comment this line, we will have problems in our screens at the time someone tries to go back
+            SetCurrentScreen(previousScreens[previousScreens.Count - 1]);
+            previousScreens.RemoveAt(previousScreens.Count - 1);
+            currentScreen.Show();
+        }
+    }
+
+    public void HideScreen()
+    {
+        currentScreen.Hide();
+    }
+    public void SetCurrentScreen(IScreen screen)
+    {
+        currentScreen = screen;
+    }
+
+    public IScreen GetCurrentScreen()
+    {
+        return currentScreen;
+    }
+
+    public IScreen GetPreviousScreen()
+    {
+        return previousScreens[previousScreens.Count - 1];
     }
 }
